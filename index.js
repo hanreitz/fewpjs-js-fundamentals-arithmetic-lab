cedfcdc6b3f9376ce556596c0456c529f327ccd4
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-06-03 17:56:00 -0400

    done'

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+const num3 = 8;
+const num4 = 12;
+
+const multiply = num1*num2;
+
+const random = Math.ceil(Math.random() * 100) + 1;
+
+const mod = num4 % num3;
+
+const max = Math.max(4,15,20,13,2);
